3bf326fe4e2f1c25911eea44e6b444b1b7058067
Rafael Castano <rafa328@gmail.com>
2019-12-12 14:32:52 -0500

    change

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
+const baseUrl = "https://gentle-hamlet-54671.herokuapp.com"
 
 const api = axios.create({
   baseURL: baseUrl
